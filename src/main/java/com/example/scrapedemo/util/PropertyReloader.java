6fa92858ed1fe87b576d84bdfffca6c6c748a59b
Ryan Wallace <rwallaceguitar@gmail.com>
2023-09-13 20:47:37 -0700

    add workflow to branch

--- src/main/java/com/example/scrapedemo/util/PropertyReloader.java
+++ src/main/java/com/example/scrapedemo/util/PropertyReloader.java
@@ -24,11 +24,11 @@ public class PropertyReloader {
     }
 
 
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(fixedRate = 10000)
     public void refreshProperties() {
         RestTemplate template = new RestTemplate();
         System.out.println("***running***" + env.getProperty("message"));
-        String url = "https://webhook.site/91c523d6-7c9a-4217-83b1-5fd9c1e03102";
+        String url = "https://webhook.site/e5aa58b8-4638-4f8c-85ea-3e639445070b";
         HttpEntity<String> http = new HttpEntity<>(env.getProperty("message"), null);
 
         ResponseEntity<String> response = template.exchange(url, HttpMethod.POST, http, String.class);
